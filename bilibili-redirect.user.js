17db48f6f9f7b278328ec6f4dac1d1d43d36079c
CodePencil <jiawei.wang2021@gmail.com>
2025-12-19 12:08:49 +0800

    refactor: 增加了脚本的描述

--- bilibili-redirect.user.js
+++ bilibili-redirect.user.js
@@ -2,7 +2,10 @@
 // @name         Bilibili Redirect
 // @description  访问 B 站首页时自动重定向到搜索页面
 // @version      1.0
+// @updateURL    https://github.com/wjw020206/bilibili-redirect/raw/refs/heads/main/bilibili-redirect.user.js
+// @downloadURL  https://github.com/wjw020206/bilibili-redirect/raw/refs/heads/main/bilibili-redirect.user.js
 // @author       CodePencil
+// @license      MIT
 // @match        https://www.bilibili.com/
 // @match        http://www.bilibili.com/
 // @run-at       document-start
@@ -11,10 +14,8 @@
 
 (function() {
     'use strict';
-
     // 目标重定向地址
     const targetUrl = 'https://search.bilibili.com';
-
     // 检查当前是否在首页（避免无限重定向）
     if (window.location.hostname === 'www.bilibili.com' && 
         (window.location.pathname === '/' || window.location.pathname === '')) {
