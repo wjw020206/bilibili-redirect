07752d7b43285366f78be4cf5d6786adea6b06b5
CodePencil <jiawei.wang2021@gmail.com>
2026-07-31 08:52:21 +0800

    feat: 完善了路径的判断逻辑

--- bilibili-redirect.user.js
+++ bilibili-redirect.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bilibili Redirect
 // @description  访问 B 站首页时自动重定向到搜索页面
-// @version      1.0
+// @version      1.1.0
 // @updateURL    https://github.com/wjw020206/bilibili-redirect/raw/refs/heads/main/bilibili-redirect.user.js
 // @downloadURL  https://github.com/wjw020206/bilibili-redirect/raw/refs/heads/main/bilibili-redirect.user.js
 // @author       CodePencil
@@ -17,7 +17,7 @@
   // 目标重定向地址
   const targetUrl = 'https://search.bilibili.com';
   // 检查当前是否在首页（避免无限重定向）
-  if (window.location.hostname === 'www.bilibili.com' && (window.location.pathname === '/' || window.location.pathname === '')) {
+  if (window.location.hostname === 'www.bilibili.com' && (['/', '', '/index.html'].includes(window.location.pathname))) {
     window.location.replace(targetUrl);
   }
 })();
